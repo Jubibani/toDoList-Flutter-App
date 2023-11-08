13a06f7a3554904b74d01d8f5d87039319da9b18
Jubibani <strawberryloli3@gmail.com>
2023-11-07 19:21:41 -0800

    changed concept to do list

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,13 @@ void main() {
     MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("DataBase")),
-      ),
+          title: const Center(
+              child: Text("To do", style: TextStyle(color: Colors.black))),
+          backgroundColor: Colors.yellow),
+      body: const Text(
+          "- each lists that are crossed out are your small wins, and small wins makes you believe in yourself"),
+      backgroundColor: Colors.white,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     )),
   );
 }
