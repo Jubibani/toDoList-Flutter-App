053c1c0410ddd038d450c7a7c4262d8f41318742
Jubibani <strawberryloli3@gmail.com>
2023-11-20 14:23:52 -0800

    added settings.json

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,16 @@ void main() {
   runApp(
     MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-          title: const Center(
-              child: Text("To do", style: TextStyle(color: Colors.black))),
-          backgroundColor: Colors.yellow),
-      body: const Text(
-          "- each lists that are crossed out are your small wins, and small wins makes you believe in yourself"),
-      backgroundColor: Colors.white,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    )),
+          appBar: AppBar(
+              title: Center(
+                  child: Text("To do", style: TextStyle(color: Colors.black))),
+              backgroundColor: Colors.yellow),
+          body: Text(
+              "- each lists that are crossed out are your small wins, and small wins makes you believe in yoursel -"),
+          backgroundColor: Colors.white,
+          floatingActionButton: FloatingActionButton(
+            child: Text('add'),
+          ),
+        )),
   );
 }
