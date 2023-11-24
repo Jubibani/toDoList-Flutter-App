c5d3b93da0b742204cf233eee5d797fbd99e5147
Jubibani <strawberryloli3@gmail.com>
2023-11-23 23:19:36 -0800

    png_icon and widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,61 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyWidget()
+  );
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-              backgroundColor:const Color.fromARGB(75, 57, 239, 100),
+              backgroundColor: Colors.indigo[500],
 
               title: const Center(
                   child: Text(
-                    "To do", 
+                    "To do: ", 
                     style: TextStyle(color: Colors.white)
                   ),
                 ),
-
               ),
 
-          body: Container(
-            child: const Center(
-              child: 
-                Text(
-                  "Just Do it", 
-                  style:TextStyle(color: Colors.black)
+          body: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Center(
+                      child: Text(
+                          "Just Do it", 
+                          style:TextStyle(color: Colors.white)
+                          ),
+                    ),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.indigo[300],
+                    borderRadius: BorderRadius.circular(10.0)
                   ),
+                  
+                  padding: const EdgeInsets.all(20.0),
+                  height: 500.0,
+                  width: 350.0,
+                  ),
+                ]
             ),
           ),
-        )
-      ),
-  );
+          bottomNavigationBar: BottomAppBar(
+            child: IconButton(
+              color: Colors.white,
+              icon: Icon(Icons.add),
+              onPressed: () {},
+              ),
+            color: Colors.indigo[500],
+            height: 50.0,
+            ),
+            ), 
+        );
+  }
 }
