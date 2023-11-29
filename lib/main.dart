ceba36b975c4fffae691059977c74927983f6487
Jubibani <strawberryloli3@gmail.com>
2023-11-29 11:12:19 -0800

    added new home layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print, sort_child_properties_last
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,42 +12,44 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.indigo[500],
           title: const Center(
-            child: Text("To do: ", style: TextStyle(color: Colors.white)),
+            child: Text("My List", style: TextStyle(color: Colors.white)),
           ),
         ),
         body: Center(
           child: Column(children: <Widget>[
             Container(
-              child: Center(
-                child: Text(
-                  "Just Do it",
-                   style: TextStyle(color: Colors.white)
-                   ),
+              child: const Center(
+                child:
+                    Text("Just Do it", style: TextStyle(color: Colors.white)),
               ),
               decoration: BoxDecoration(
                   shape: BoxShape.rectangle,
                   color: Colors.indigo[300],
                   borderRadius: BorderRadius.circular(10.0)),
-              padding: const EdgeInsets.all(20.0),
               height: 500.0,
               width: 350.0,
             ),
           ]),
         ),
-        bottomNavigationBar: BottomAppBar(
-          child: IconButton(
-            color: Colors.white,
-            icon: Icon(Icons.add),
+        floatingActionButton: 
+          FloatingActionButton(
             onPressed: () {
-              print("button pressed to add");
+              debugPrint('pressedFloatingActionButton');
             },
+            child: const Icon(Icons.add),
+          backgroundColor: Colors.indigo[500],
           ),
-          color: Colors.indigo[500],
-          height: 50.0,
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+          NavigationDestination(icon: Icon(Icons.settings), label: "settings"),
+           NavigationDestination(icon: Icon(Icons.person), label: "profile"),
+          ],
+        backgroundColor: Colors.indigo[500],
         ),
       ),
     );
